8939c91bc2dec8f8b074d1cc0728f458d29b483d
Naveen3990 <sharmivarun3990@gmail.com>
2026-06-27 15:40:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "varunsujith9966"      # your S3 bucket name
+    key    = "terraform.tfstate"    # file name/path inside the bucket
+    region = "us-east-1"            # must match your AWS region
+  }
+}
+
 provider "aws" {
   region = "us-east-1"
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-08f44e8eca9095668"
+  ami           = "ami-0f58b397bc5c1f2e8"
   instance_type = "t3.micro"
 
   tags = {
