7b52e4e610adb71bb8a5716673602d808c9db85f
Naveen3990 <sharmivarun3990@gmail.com>
2026-06-27 15:27:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0f58b397bc5c1f2e8"
+  ami           = "ami-08f44e8eca9095668"
   instance_type = "t3.micro"
 
   tags = {
